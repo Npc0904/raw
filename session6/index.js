42f7ce77315814b6f5c8c8e20ec81af95ca0d8c6
TuNPAC <Quoctu090401@gmail.com>
2023-09-19 00:54:37 +0900

    update
    update

---
+++ session6/index.js
@@ -0,0 +1,23 @@
+/* 
+    comment code nhiều dòng
+    dòng 1
+    dòng 2
+    dòng 3
+ */
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ *  */
+// 
+// comment 1 dòng
+
+let a = 6;
+let b = 7;
